dd4723c94d3a5ce46b846c7a74303aa416fc281f
Chrystiam Andrade <chrystiam.andrade@gmail.com>
2018-02-01 15:13:29 +0000

    Aula 08-09 | Configurando Devise e Criado o model Category

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cadastrando Categorias ... "
+categories = [ "Animais e acessórios",
+                "Esportes",
+                "Para sua casa",
+                "Eletrônicos e celulares",
+                "Música e hobbies",
+                "Veículos e barcos",
+                "Imóveis",
+                "Empregos e negócios"]
+
+categories.each do |c|
+    Category.find_or_create_by(description: c)
+end
+
+puts "Categorias cadastradas com sucesso ... "
